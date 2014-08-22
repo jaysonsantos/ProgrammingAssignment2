0eae1dd81088e1316a2f2279fbb079fc1b1f81c1
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2014-08-21 21:26:08 -0300

    created functions to make inverse matrices cacheable

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    cache <- NULL
+    set <- function (y) {
+        x <<- y
+        cache <<- NULL
+    }
 
+    get <- function () x
+
+    setInverse <- function(inverse) cache <<- inverse
+    getInverse <- function() cache
+
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse = x$getInverse()
+    if (!is.null(inverse)) {
+        message('Matrix inverse already calculated.')
+        return(inverse)
+    }
+
+    message('Matrix inverse not calculated yet, calculating it now.')
+    matrixData <- x$get()
+    inverseMatrix <- solve(matrixData)
+    x$setInverse(inverseMatrix)
+    inverseMatrix
 }
+
+# Creates a matrix and inverse it to check against cache
+matrix1 <- matrix(c(2, 4, 6, 8), 2, 2)
+inverse <- solve(matrix1)
+
+cacheableMatrix1 <- makeCacheMatrix(matrix1)
+inverseMatrix <- cacheSolve(cacheableMatrix1)
+
+# Just check if values are the same
+print(inverseMatrix == inverse)
+
+# Checking if it is now came from cache
+inverseMatrix <- cacheSolve(cacheableMatrix1)
+print(inverseMatrix == inverse)
+
+
+
